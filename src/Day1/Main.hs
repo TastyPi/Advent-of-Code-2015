714fb3ebe0018c7c3ac148d5c334298ceea06914
Graham Rogers <graham@rogers.me.uk>
2020-12-02 01:01:14 +0000

    SOlution to day 1 part 2

--- src/Day1/Main.hs
+++ src/Day1/Main.hs
@@ -2,39 +2,30 @@
 module Day1.Main where
 
 import Control.Applicative
-import Control.Monad
-import Data.Attoparsec.ByteString.Char8 (char)
-import Data.Attoparsec.ByteString.Lazy
-import Data.ByteString.Lazy
+import Data.Attoparsec.ByteString.Char8 (Parser, char, parseOnly)
+import Data.ByteString (hGetContents)
 import Data.Functor
 import System.IO (IOMode(ReadMode), withFile)
 
-newtype Instruction a = Delta a
-  deriving Show
+up :: Num a => Parser a
+up = char '(' $> 1
 
-instance Num a => Semigroup (Instruction a) where
-  (Delta x) <> (Delta y) = Delta $ x + y
+down :: Num a => Parser a
+down = char ')' $> -1
 
-instance Num a => Monoid (Instruction a) where
-  mempty = Delta 0
-
-mkInstruction :: a -> Instruction a
-mkInstruction = Delta
-
-up :: Num a => Parser (Instruction a)
-up = char '(' $> mkInstruction 1
-
-down :: Num a => Parser (Instruction a)
-down = char ')' $> mkInstruction (-1)
-
-instruction :: Num a => Parser (Instruction a)
+instruction :: Num a => Parser a
 instruction = up <|> down
 
-instructions :: Num a => Parser [Instruction a]
+instructions :: Num a => Parser [a]
 instructions = many instruction
 
-withInput :: (ByteString -> IO a) -> IO a
-withInput f = withFile "data/Day1/input.txt" ReadMode $ hGetContents >=> f
+withInput :: Num a => (String -> IO b) -> ([a] -> IO b) -> IO b
+withInput onError onSuccess = withFile "data/Day1/input.txt" ReadMode $ \handle -> do
+  contents <- hGetContents handle
+  let result = parseOnly instructions contents
+  either onError onSuccess result
 
 main :: IO ()
-main = withInput $ print . fmap mconcat . parse (instructions @Int)
+main = withInput @Int print $ \input -> do
+  putStrLn $ "Part 1: " ++ show (sum input)
+  putStrLn $ "Part 2: " ++ show (length $ takeWhile (>=0) $ scanl (+) 0 input)
